bfc47f263d4461cc202af820bf89e95af3d417e5
Gionata Massi (in qualità di docente d'Informatica) <gionata.massi@savoiabenincasa.it>
2024-11-30 12:55:40 +0100

    Create login_ricerca_binaria.cpp

---
+++ 3asia/2024-11-30/login_ricerca_binaria.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Account {
+    string username;
+    string password;
+};
+
+const int N = 5;
+Account as[N] = {
+    { "clarabella", "muuuu" },
+    { "micky", "minnie" },
+    { "minnie", "topolino"},
+    { "paperino", "quack" },
+    { "pluto", "bau"}
+};
+Account a { "micky", "minnie" };
+
+string login_seq(Account &a, Account as[], int n) {
+    for (int i = 0; i < n; ++i) {
+        if (a.username == as[i].username &&
+            a.password == as[i].password)
+                return "Login!";
+    }
+    return "Impossibile eseguire l'accesso";
+}
+
+string login_bin_r(Account &a, Account as[], int begin, int end) {
+    if (begin >= end)
+        return "Impossibile eseguire l'accesso";
+    int center = (begin + end) / 2;
+    if (a.username == as[center].username)
+            if (a.password == as[center].password)
+                return "Login!";
+            else
+                return "Impossibile eseguire l'accesso";
+    if (a.username < as[center].username)
+        return login_bin_r(a, as, begin, center);
+    else
+        return login_bin_r(a, as, center + 1, end);
+        
+}
+
+string login_bin_i(Account &a, Account as[], int end) {
+    int begin = 0;
+    
+    while (begin < end) {
+        int center = (begin + end) / 2;
+        if (a.username == as[center].username)
+                if (a.password == as[center].password)
+                    return "Login!";
+                else
+                    return "Impossibile eseguire l'accesso";
+        if (a.username < as[center].username)
+            end = center;
+        else
+            begin = center + 1;
+    }
+    return "Impossibile eseguire l'accesso";
+}
+
+
+int main() {
+    cout << "username: ";
+    cin >> a.username;
+    cout << "password: ";
+    cin >> a.password;
+
+    cout << login_seq(a, as, N) << "\n";
+    cout << login_bin_i(a, as, 0, N) << "\n";
+    cout << login_bin_i(a, as, N) << "\n";
+    
+    return 0;
+}
